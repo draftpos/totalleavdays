c0e32b1c98de77d6ed1326f6ce273385e461e397
wsidommapeka <wisdommapeka@gmail.com>
2025-09-04 08:33:31 +0200

    fix

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Total Leave Days',  # Required: Name of your package
+    name='total-leave-days',  # Use hyphens instead of spaces
     version='0.1.0',          # Required: Package version
-    author='wiz',       # Optional: Author name
+    author='Wiz',             # Optional: Author name (capitalize if desired)
     author_email='wisdommapeka@gmail.com',  # Optional: Author email
-    description='Total Number Of Leave Days Calculation',  # Optional: Description
-    long_description=open('README.md').read(),  # Optional: Long description from README
+    description='Total Number of Leave Days Calculation',  # Optional: Description
+    long_description=open('README.md').read(),  # Ensure README.md exists
     long_description_content_type='text/markdown',  # Optional: Format of long description
-    url='https://github.com/WisdomMapeka/totalleavdays.git',  # Optional: URL for the project
+    url='https://github.com/WisdomMapeka/totalleavdays',  # Ensure the URL is correct
     packages=find_packages(),  # Automatically find packages in the directory
     classifiers=[  # Optional: Classifiers for PyPI
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.10',  # Optional: Specify Python version
-    install_requires=[  # Optional: List of dependencies
-    ],
+    install_requires=[],  # List of dependencies (add as needed)
 )
